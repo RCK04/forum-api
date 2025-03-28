de9655c2d4e69352716884adc2def346aafcc746
Erick <erickkan2004@gmail.com>
2025-03-27 21:19:56 -0300

    feat: Auth Guard

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,35 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  @Inject()
+  private readonly jwtService: JwtService;
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authorization = this.extractTokenFromHeader(request);
+    if (!authorization) throw new UnauthorizedException('Unauthorized');
+
+    try {
+      const payLoad = this.jwtService.verify(authorization);
+      request.userId = payLoad;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] =
+      request.headers.get('authorization')?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
