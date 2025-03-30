84fe830aa0cdafd72f22a4eefce31872fc2dc89b
Erick <erickkan2004@gmail.com>
2025-03-29 23:50:11 -0300

    Comments

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/common';
 import { Prisma, User as UserModel } from '@prisma/client';
 import { UserService } from './user.service';
-// Rota principal
+// Rota principal user
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
